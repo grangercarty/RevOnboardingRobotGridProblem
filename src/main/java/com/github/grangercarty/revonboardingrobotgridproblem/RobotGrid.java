343d45685762ed8ec6b128edd11c0236fb48b259
Granger Carty <grangercarty@gmail.com>
2022-04-28 15:18:05 -0500

    Finished findPath

--- src/main/java/com/github/grangercarty/revonboardingrobotgridproblem/RobotGrid.java
+++ src/main/java/com/github/grangercarty/revonboardingrobotgridproblem/RobotGrid.java
@@ -24,29 +24,55 @@ public class RobotGrid {
         offLimitCells.add(offLimitCell);
     }
 
+    public boolean isOffLimitCell(int row, int column) {
+        boolean isOffLimits = false;
+        for (int[] cell : offLimitCells) {
+            isOffLimits = (isOffLimits || (cell[0] == row && cell[1] == column));
+        }
+        return isOffLimits;
+    }
+
     public boolean findPath() {
         int[] location = new int[2];
         location[0] = 1;
         location[1] = 1;
-        pathExists = !offLimitCells.contains(location);
+        pathExists = !isOffLimitCell(location[0],location[1]);
         String direction = "Down";
         while (pathExists && (location[0] < numRows || location[1] < numColumns)) {
             if (direction.equals("Down")) {
-                if (location[0] < numRows) {
+                if ( location[0] >= numRows || isOffLimitCell(location[0]+1, location[1]) ) {
+                    direction = "Right";
+                }
+                else {
                     robotPath.add(direction);
                     location[0]++;
                 }
-                else {
-                    direction = "Right";
-                }
             }
             else if (direction.equals("Right")) {
-                robotPath.add(direction);
-                location[1]++;
+                if ( location[1] >= numRows || isOffLimitCell(location[0], location[1]+1) ) {
+                    direction = "Back";
+                }
+                else {
+                    robotPath.add(direction);
+                    location[1]++;
+                    direction = "Down";
+                }
             }
             else {
                 if (robotPath.size() == 0) {
                     pathExists = false;
+                }
+                else {
+                    direction = robotPath.get(robotPath.size()-1);
+                    robotPath.remove(robotPath.size()-1);
+                    if (direction.equals("Down")) {
+                        direction = "Right";
+                        location[0]--;
+                    }
+                    else {
+                        direction = "Back";
+                        location[1]--;
+                    }
                 }
             }
         }
--- src/test/java/com/github/grangercarty/revonboardingrobotgridproblem/RobotGridTest.java
+++ src/test/java/com/github/grangercarty/revonboardingrobotgridproblem/RobotGridTest.java
@@ -13,5 +13,18 @@ public class RobotGridTest {
         RobotGrid roboGrid2 = new RobotGrid(2,2);
         Assertions.assertTrue(roboGrid2.findPath());
         System.out.println(roboGrid2.printPath());
+
+        RobotGrid roboGrid3 = new RobotGrid(2,2);
+        roboGrid3.addOffLimitCell(2,1);
+        Assertions.assertTrue(roboGrid3.isOffLimitCell(2,1));
+        Assertions.assertTrue(roboGrid3.findPath());
+        System.out.println(roboGrid3.printPath());
+
+        RobotGrid roboGrid4 = new RobotGrid(4,4);
+        roboGrid4.addOffLimitCell(4,1);
+        roboGrid4.addOffLimitCell(3,2);
+        roboGrid4.addOffLimitCell(2,3);
+        Assertions.assertTrue(roboGrid4.findPath());
+        System.out.println(roboGrid4.printPath());
     }
 }
