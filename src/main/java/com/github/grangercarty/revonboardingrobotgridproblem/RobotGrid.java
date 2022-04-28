887c6a5ecfcdf5186faa8a722f131b908689b54f
Granger Carty <grangercarty@gmail.com>
2022-04-28 09:36:31 -0500

    Initial commit for robot grid problem

---
+++ src/main/java/com/github/grangercarty/revonboardingrobotgridproblem/RobotGrid.java
@@ -0,0 +1,11 @@
+package com.github.grangercarty.revonboardingrobotgridproblem;
+
+public class RobotGrid {
+    int numRows;
+    int numColumns;
+
+    public RobotGrid(int rows, int columns) {
+        this.numRows = rows;
+        this.numColumns = columns;
+    }
+}
---
+++ src/test/java/com/github/grangercarty/revonboardingrobotgridproblem/RobotGridTest.java
@@ -0,0 +1,11 @@
+package com.github.grangercarty.revonboardingrobotgridproblem;
+
+import org.junit.jupiter.api.Test;
+
+public class RobotGridTest {
+
+    @Test
+    public void findPathTest() {
+        RobotGrid roboGrid = new RobotGrid(1, 1);
+    }
+}
